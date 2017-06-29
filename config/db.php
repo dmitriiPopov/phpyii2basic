5781791b699a4e313fd5422e409dbeb5765aaade
dmitriipopov90 <dmitriipopov90@gmail.com>
2017-06-29 13:36:22 +0300

    First commit: add base structure

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class'    => 'yii\db\Connection',
+    'dsn'      => 'mysql:host=localhost;dbname=yii2-basic',
+    'username' => 'root',
+    'password' => '2824562',
+    'charset'  => 'utf8',
+];
